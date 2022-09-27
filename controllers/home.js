bfbc0952b18ae6dc8ade379a76461158acd41cda
racheleckenrod <rachel@racheleckenrod.com>
2022-09-27 11:25:09 -0500

    updated positive changes from commit 0fcb8f48783

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,14 @@
+const Post = require("../models/Post");
+const Comment = require("../models/Comment")
+
 module.exports = {
-    getIndex: (req, res) => {
-      res.render("index.ejs");
-    },
-  };
-  
+    getIndex: async (req, res) => {
+      try {
+        const posts = await Post.find().populate('user').sort({ likes: "desc" }).lean();
+        const comments = await Comment.find().sort({ createdAt: "asc" }).lean()
+        res.render("index.ejs", { posts: posts, comments: comments } );
+      } catch (err) {
+        console.log(err)
+      }
+    }
+}
